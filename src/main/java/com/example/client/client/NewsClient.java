80dc9510ac0c032956c5e9cce4a0a87a75322546
aleksandrtufanov <2020tufanov@gmail.com>
2025-02-28 17:20:33 +0300

    JavaDoc в Клиент

--- src/main/java/com/example/client/client/NewsClient.java
+++ src/main/java/com/example/client/client/NewsClient.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-
+/**
+ * Клиент для взаимодействия с API новостей.
+ * Используется для получения списка всех новостей с удалённого сервиса.
+ */
 @FeignClient(name = "messageClient", url = "https://tender-heads-bathe.loca.lt/news")
 public interface NewsClient {
 
